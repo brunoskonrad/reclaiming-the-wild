b3e98ce2634296975a5c9df1b2c4e446e0f93b74
Bruno Konrad <bruno.skon@gmail.com>
2023-06-23 16:58:06 +0200

    Add authentication with Discord

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,18 @@
+import { prisma } from "@/lib/db";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import NextAuth, { NextAuthOptions } from "next-auth";
+import { Adapter } from "next-auth/adapters";
+import DiscordProvider from "next-auth/providers/discord";
+
+export const authOptions: NextAuthOptions = {
+  // TODO: eventually check why the Adapter types aren't matching
+  adapter: PrismaAdapter(prisma) as Adapter,
+  providers: [
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
