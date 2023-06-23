732d615bb70297821a05f7f884c1bf4973138962
Bruno Konrad <bruno.skon@gmail.com>
2023-06-23 17:21:13 +0200

    Log current session

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
 import { prisma } from "@/lib/db";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
 
 export default async function Home() {
   const koroks = await prisma.koroks.findMany();
-  getServerSession();
+  const session = await getServerSession(authOptions);
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <strong>{JSON.stringify(koroks, null, 2)}</strong>
+      <Print object={koroks} />
+
+      <Print object={session} />
     </main>
   );
+}
+
+function Print({ object }: { object: any }) {
+  if (!object) {
+    return (
+      <span>
+        Object is <code>null</code> or <code>undefined</code>
+      </span>
+    );
+  }
+
+  return <strong>{JSON.stringify(object, null, 2)}</strong>;
 }
